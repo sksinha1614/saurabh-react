7a9e4a9d0816c027d4753b6c8f9739bad9f8e1f0
sksinha1614 <sksinha1614@gmail.com>
2026-07-05 11:09:45 +0530

    app ignited

--- App.js
+++ App.js
@@ -1,18 +1,15 @@
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "abc" },
-//   "hello world from React",
-// );
-// const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+  React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement("h1", { key: "h1-1" }, "I am h1 tag"),
+    React.createElement("h2", { key: "h2-1" }, "I am h2 tag"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", { key: "h1-2" }, "I am h1 tag"),
+    React.createElement("h2", { key: "h2-2" }, "I am h2 tag"),
   ]),
 ]);
 
