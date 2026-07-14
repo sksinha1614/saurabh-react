c8c943732226a91fe3070c6124d98d61c04018ef
sksinha1614 <sksinha1614@gmail.com>
2026-07-14 12:48:34 +0530

    structured folder with hooks

---
+++ src/Ep1-2/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement("h1", { key: "h1-1" }, "I am h1 tag"),
+    React.createElement("h2", { key: "h2-1" }, "I am h2 tag"),
+  ]),
+
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", { key: "h1-2" }, "I am h1 tag"),
+    React.createElement("h2", { key: "h2-2" }, "I am h2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
